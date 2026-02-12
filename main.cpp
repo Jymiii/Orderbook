ce188f22bf1058d22ea6223e0ae16abbb1c470f9
Jimi van der Meer <J.F.vanderMeer@student.tudelft.nl>
2026-02-12 14:21:47 +0000

    test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,51 @@
-#include "Orderbook/Orderbook.h"
+#include "orderbook/Orderbook.h"
 #include <iostream>
 #include <thread>
+#include <chrono> // for std::chrono functions
+
+class Timer
+{
+private:
+    // Type aliases to make accessing nested type easier
+    using Clock = std::chrono::steady_clock;
+    using Second = std::chrono::duration<double, std::ratio<1> >;
+
+    std::chrono::time_point<Clock> m_beg { Clock::now() };
+
+public:
+    void reset()
+    {
+        m_beg = Clock::now();
+    }
+
+    double elapsed() const
+    {
+        return std::chrono::duration_cast<Second>(Clock::now() - m_beg).count();
+    }
+};
+
 
 int main() {
     Orderbook ob{};
+    std::vector<OrderPtr> buys;
+    std::vector<OrderPtr> sells;
 
-    ob.addOrder(std::make_shared<Order>(1, OrderType::GoodForDay, Side::Buy, 100, 10));
-    std::cout << "Before: " << ob.size() << "\n";
+    buys.reserve(100000000);
+    for (size_t i = 0; i < 100000000; i++){
+        buys.emplace_back(std::make_shared<Order>(static_cast<OrderId>(i), OrderType::GoodTillCancel, Side::Buy, 50, 10));
+    }
+    sells.reserve(10000000);
+    for (size_t i = 0; i < 10000000; i++){
+        sells.emplace_back(std::make_shared<Order>(static_cast<OrderId>(i), OrderType::GoodTillCancel, Side::Sell, 50, 100));
+    }
 
-    // Wait long enough for your prune to trigger (only practical if close is soon)
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-
-    std::cout << "After: " << ob.size() << "\n";
+    std::cout << "starting" << std::endl;
+    Timer timer;
+    for (const auto& o : buys){
+        ob.addOrder(o);
+    }
+    for (const auto& o : sells){
+        ob.addOrder(o);
+    }
+    std::cout << timer.elapsed();
 }
