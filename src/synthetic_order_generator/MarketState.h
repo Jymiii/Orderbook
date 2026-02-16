15455bd3181782d5602fb19ffdf9ce7f5847685d
Jimi van der Meer <J.F.vanderMeer@student.tudelft.nl>
2026-02-16 17:04:01 +0000

    dt changes

--- src/synthetic_order_generator/MarketState.h
+++ src/synthetic_order_generator/MarketState.h
@@ -8,7 +8,7 @@ struct MarketState {
     double mid = 100.0;
     double drift = 0.1;
     double sigma = 0.2;
-    double dt = 0.00000001;
+    double dt = 0.0001;
     double b = 0.002;
 };
 #endif //ORDERBOOK_MARKETSTATE_H
